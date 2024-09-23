ea1d42b38828c05e6e845896f61d5ad253ebab44
Zero0242 <pedrocorream66@gmail.com>
2024-09-23 19:22:00 -0300

    [REWRITE]: versión 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,20 @@
-import 'package:bases_web/router/router.dart';
 import 'package:flutter/material.dart';
-import 'package:bases_web/locator.dart';
-import 'package:bases_web/services/navigation_service.dart';
-import 'package:bases_web/ui/layouts/main_layout_page.dart';
 
 void main() {
-  setupLocator();
-  WebAppRouter.configureRoutes();
-  runApp(const MyApp());
+  runApp(const MainApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Web Test App',
-      initialRoute: '/',
-      //onGenerateRoute: RouteGenerator.generateRoute,
-      onGenerateRoute: WebAppRouter.router.generator,
-      builder: (_, child) => MainLayoutPage(
-        child: child ?? const CircularProgressIndicator(),
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World!'),
+        ),
       ),
-      navigatorKey: locator<NavigationService>().navigatorKey,
     );
   }
 }
